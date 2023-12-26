41f973d7cd5bc66537dd9fa61adfa89280abc788
helloapricity <140663414+helloapricity@users.noreply.github.com>
2023-12-26 16:52:05 +0700

    Update Driver_raw_dataset.py

---
+++ Dataset/Raw Dataset/Driver_raw_dataset.py
@@ -0,0 +1,4 @@
+"""
+Link Driver Raw Dataset:
+https://drive.google.com/drive/folders/1G4MJ83D67uTnywY_pVz4anVZJOpX60f3?usp=sharing
+"""
--- Dataset/Raw Dataset/Driver_raw_field_dataset.py
+++
@@ -1,4 +0,0 @@
-"""
-Link Driver Raw Field Dataset:
-https://drive.google.com/drive/folders/11jW3d5PE7QMnBgFXsRXdcqgo2xnv-zqm?usp=sharing
-"""
